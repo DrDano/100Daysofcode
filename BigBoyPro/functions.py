716f9fe1a73d0574e864dac69d35edc71c238a55
DrDano <danielharned@gmail.com>
2021-06-25 15:25:37 -0700

    "Modifications on 062521 2"

--- BigBoyPro/functions.py
+++ BigBoyPro/functions.py
@@ -3,6 +3,7 @@ from data import MENU, resources
 
 serving = True
 runs = 0
+profit = 0
 water = resources["water"]
 milk = resources["milk"]
 coffee = resources["coffee"]
@@ -20,7 +21,8 @@ while serving == True:
     print(logo)
     f.close()
 
-    print(f"Menu ~ Espresso: ${MENU['espresso']['cost']}0, Latte: ${MENU['latte']['cost']}0 Cappuccino: ${MENU['cappuccino']['cost']}0 ~")
+    print(f"Menu ~ Espresso: ${MENU['espresso']['cost']}0, Latte: ${MENU['latte']['cost']}"
+          f"0 Cappuccino: ${MENU['cappuccino']['cost']}0 ~")
     user_prompt = input("What would you like? (espresso/latte/cappuccino): ").lower()
 
     # TODO create function to report resources
@@ -29,9 +31,10 @@ while serving == True:
             print(f"water remaining: {water_rem}\nmilk remaining: {milk_rem}\ncoffee remaining: {coffee_rem}")
     if user_prompt == "report":
         report(water_rem, milk_rem, coffee_rem)
+        print(f"Profit: ${profit}")
 
     # TODO create function to turn off machine
-    elif user_prompt == "exit":
+    elif user_prompt == "off":
         pass
 
     else:
@@ -50,40 +53,41 @@ while serving == True:
 
         # TODO calculate and display change if any
         item_cost = MENU[user_prompt]["cost"]
-        total_change = total_funds - item_cost * 100
+        profit += item_cost
+        total_change = total_funds - (item_cost * 100)
         money = True
         if total_change <= 0:
             print("Sorry, not enough funds to complete transaction.")
             money = False
 
-        def change_calc():
-            """takes the remainder of the total change after dividing by each form of currency and returns quantity of 'quarters', 'dimes', etc."""
-            change_rem = total_change % 25
-            quar_num = int(round(total_change / 25, 0))
-            dime_num = int(round(change_rem / 10, 0))
-            change_rem % 10
+        def change_calc(change):
+            """takes the remainder of the total change after dividing by each form of
+            currency and returns quantity of 'quarters', 'dimes', etc."""
+            quar_num = int(change / 25)
+            change_rem = change % 25
+            dime_num = int(change_rem / 10)
+            if change_rem / 10 > 1:
+                change_rem %= 10
+            else:
+                dime_num = 0
             if change_rem / 5 > 1:
-                nickel_num = int(round(change_rem / 5, 0))
+                nickel_num = int(change_rem / 5)
             else:
                 nickel_num = 0
-            change_rem % 5
-            penny_num = int(round(change_rem / 1, 0))
-            change_rem % 1
+            change_rem %= 5
+            penny_num = int(change_rem / 1)
+            change_rem %= 1
             return quar_num, dime_num, nickel_num, penny_num
 
-        # TODO determine if coffee machine has enough supplies for drink and refund user money if not
-
+        # TODO determine if coffee machine has enough supplies, deduct those supplies for drink,
+        #  and refund user money if not enough supplies.
         supply = True
         if water_rem - MENU[user_prompt]["ingredients"]["water"] > 0:
             water_rem -= MENU[user_prompt]["ingredients"]["water"]
-        else:
-            supply = False
         if user_prompt == "espresso":
             pass
         elif milk_rem - MENU[user_prompt]["ingredients"]["milk"] > 0:
             milk_rem -= MENU[user_prompt]["ingredients"]["milk"]
-        else:
-            supply = False
         if coffee_rem - MENU[user_prompt]["ingredients"]["coffee"] > 0:
             coffee_rem -= MENU[user_prompt]["ingredients"]["coffee"]
         else:
@@ -93,16 +97,16 @@ while serving == True:
             print("Machine has not enough resources for drink.")
             print(f"You have been refunded ${total_funds / 100}")
 
-        if supply == True and money == True:
+        if supply and money:
             print(f"Here is your {user_prompt}, enjoy!")
-            change_cats = [{"quarters": change_calc()[0]}, {"dimes": change_calc()[1]}, {"nickels": change_calc()[2]}, {"pennies": change_calc()[3]}]
-            print(f"Your change is: {change_cats[0]['quarters']} Quarters, {change_cats[1]['dimes']} Dimes, {change_cats[2]['nickels']} Nickels, and {change_cats[3]['pennies']} Pennies.")
+            print(f"Your change is: {change_calc(change = total_change)[0]} Quarters, {change_calc(change = total_change)[1]} Dimes, "
+                  f"{change_calc(change = total_change)[2]} Nickels, and {change_calc(change = total_change)[3]} Pennies.")
         else:
             pass
 
         # TODO present drink to user and subtract supplies and cost while adding profits
         # TODO Ask user if they would like another drink
-        print(f"You have ${round(total_change / 100, 2)} being refunded to you.")
+        print(f"${round(total_change / 100, 2)} was refunded to you.")
 
         repeat = input("Would you like to order something else? 'y' or 'n': ").lower()
         if repeat == "n":
