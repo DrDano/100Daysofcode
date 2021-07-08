537fabb721cdd567d3bad8f4607846472bda80d6
DrDano <danielharned@gmail.com>
2021-07-07 21:18:46 -0700

    completed Quiz game project

--- day-17-Quiz-Project/quiz-game-start/main.py
+++ day-17-Quiz-Project/quiz-game-start/main.py
@@ -4,11 +4,15 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for q in question_data:
-    question_text = q["text"]
-    question_answer = q["answer"]
+    question_text = q["question"]
+    question_answer = q["correct_answer"]
     new_question = Question(text=question_text, answer=question_answer)
     question_bank.append(new_question)
 
-
-QB = QuizBrain()
-QB.next_question(question_list=question_bank)
+QB = QuizBrain(question_bank)
+while QB.still_has_questions():
+    QB.next_question()
+    print("")
+else:
+    print("Quiz Completed!")
+    print(f"Your final score is: {QB.user_score}/{QB.question_number}")
